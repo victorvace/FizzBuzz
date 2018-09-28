706ed8419f8f81c78aec759adfe618fb4c35753d
victorvace <victorvace95@gmail.com>
2018-09-28 13:29:59 +0200

    Create methods and testing of FizzBuzz

--- src/main/java/com/vicvace/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/vicvace/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,19 @@
 package com.vicvace.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final int FIZZ_FACTOR = 3;
+    public static final int BUZZ_FACTOR = 5;
+
+    public static boolean isFizz(int value) {
+        return 0 == value % FIZZ_FACTOR;
+    }
+
+    public static boolean isBuzz(int value) {
+        return 0 == value % BUZZ_FACTOR;
+    }
+
+    public static boolean isFizzBuzz(int value) {
+        return isFizz(value) && isBuzz(value);
+    }
 }
--- src/test/java/com/vicvace/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/vicvace/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,67 @@
 package com.vicvace.fizzbuzz;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
+@RunWith(DataProviderRunner.class)
 public class FizzBuzzTest {
+
+    @DataProvider
+    public static Object[][] dataNumberProviderForFizz(){
+        return new Object[][]{
+                {3, true}, {6, true}, {9, true}, {12, true}, {15, true}, {18, true}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] dataNumberProviderForBuzz(){
+        return new Object[][]{
+                {5, true}, {10, true}, {15, true}, {20, true}, {25, true}, {30, true}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] dataNumberProviderForFizzBuzz(){
+        return new Object[][]{
+                {15, true}, {30, true}, {45, true}, {60, true}, {75, true}
+        };
+    }
 
     @Test
     public void firstTest() {
         assertTrue(true);
+    }
+
+    @Test
+    @UseDataProvider("dataNumberProviderForFizz")
+    public void itShouldReturnFizzIfDivisibleByThree(final int input, final boolean expected){
+        //Arrange Action
+        boolean result = FizzBuzz.isFizz(input);
+        //Assertion
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @UseDataProvider("dataNumberProviderForBuzz")
+    public void itShouldReturnBuzzIfDivisibleByFive(final int input, final boolean expected){
+        //Arrange Action
+        boolean result = FizzBuzz.isBuzz(input);
+        //Assertion
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @UseDataProvider("dataNumberProviderForFizzBuzz")
+    public void itShouldReturnFizzBuzzIfDivisibleByFive(final int input, final boolean expected){
+        //Arrange Action
+        boolean result = FizzBuzz.isFizzBuzz(input);
+        //Assertion
+        assertEquals(expected, result);
     }
 }
