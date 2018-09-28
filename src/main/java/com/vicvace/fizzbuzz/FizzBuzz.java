28e2ff5a3c3f51a0c09ddfb9c06873a393d095e5
victorvace <victorvace95@gmail.com>
2018-09-28 12:23:53 +0200

    First commit Develop branch

---
+++ src/main/java/com/vicvace/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,4 @@
+package com.vicvace.fizzbuzz;
+
+public class FizzBuzz {
+}
---
+++ src/test/java/com/vicvace/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.vicvace.fizzbuzz;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
